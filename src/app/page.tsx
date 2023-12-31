cd46378b1bfa4ea0b2a1617f39925d5c186b95c2
Darshan Shah <darshanshah.ds2017@gmail.com>
2023-12-31 11:06:14 +0545

    initial settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import MaxWidthWrapper from '@/components/MaxWidthWrapper'
 import Image from 'next/image'
 
 export default function Home() {
-  return <p>Hello World</p>
+  return (
+    <MaxWidthWrapper>
+      <div className='py-20 mx-auto text-center flex flex-col items-center max-w-3x'></div>
+    </MaxWidthWrapper>
+  )
 }
