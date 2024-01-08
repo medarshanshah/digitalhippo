d8f002ad1ca7870a96868d34d17c4432244a89f1
Darshan Shah <darshanshah.ds2017@gmail.com>
2024-01-08 21:24:00 +0545

    Making Navbar dynamic with user login status

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,14 @@ import { Icons } from "./Icons";
 import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
 import Cart from "./Cart";
+import { getServerSideUser } from "@/lib/payload-utils";
+import { cookies } from 'next/headers'
+import UserAccountNav from "./UserAccountNav";
 
-const Navbar = () => {
+const Navbar = async () => {
     
-    const user = null;
+    const nextCookies = cookies()
+    const { user } = await getServerSideUser(nextCookies)
     
     return (
         <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
@@ -36,7 +40,7 @@ const Navbar = () => {
                                         <span className="h-6 w-px bg-gray-200" aria-hidden='true' />
                                     )}
 
-                                    {user ? <p></p> : <Link href='/sign-up' className={buttonVariants({variant: 'ghost'})}>Create Account</Link>}
+                                    {user ? <UserAccountNav user={user}/> : <Link href='/sign-up' className={buttonVariants({variant: 'ghost'})}>Create Account</Link>}
 
                                     {user ? <span className="h-6 w-px bg-gray-200" aria-hidden='true' /> : null}
 
